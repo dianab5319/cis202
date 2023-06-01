1bbed870f46289fc980b24b555201f84b87f23d6
dianab5319 <129228502+dianab5319@users.noreply.github.com>
2023-06-01 21:41:25 +0000

    edits

--- projects/tree-project/main.cpp
+++ projects/tree-project/main.cpp
@@ -1,3 +1,11 @@
+/*
+FileName: main.cpp
+description: using a tree structure the program will input the data and then output the size as well as the contents
+             of the trees.
+Date: 6/01/23
+Author: Diana Barbosa
+email: dianab5319@student.vvc.edu
+*/
 #include "tree.h"
 #include <iostream>
 #include <string>
